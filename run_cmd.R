4cb9ea9d9e39db842d84262ac14b9c0678e7d8ae
jmmorales <pajarom@gmail.com>
2022-09-09 14:50:32 +0100

    added some analysis of results

--- run_cmd.R
+++ run_cmd.R
@@ -1,9 +1,9 @@
 
-# install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
 
 library(cmdstanr)
 check_cmdstan_toolchain(fix = TRUE, quiet = TRUE)
-#install_cmdstan()
+install_cmdstan()
 
 
 load("matrices_Joaquin_ag22.rdata")
@@ -52,13 +52,66 @@ mod <- cmdstan_model('model_joaquin_apr22.stan')
 
 fit <- mod$sample(
   data = stan_dat, 
-  seed = 123, 
+  #seed = 123, 
   chains = 4, 
   parallel_chains = 4,
-  iter_warmup = 5000,
+  output_dir = "C:\\Users\\jm361n\\uruguay",
+  iter_warmup = 2000,
   iter_sampling = 10000,
   thin = 10,
   refresh = 200 # print update every 500 iters
 )
 
+# fit_vb = mod$variational(
+#   data = stan_dat, 
+#   grad_samples = 100,
+#   seed = 123
+# )
+
+fit_summary = fit$summary(pars)
+write.csv(fit_summary, file = "fit_summary1.csv")
+save(fit_summary, file = "fit_summary.RData")
+
+# Rhat Neff ---------------------------------------------------------------
+
+op <- par(mfrow = c(1,2))
+hist(fit_summary$rhat, main = "R-hat")
+hist(fit_summary$ess_bulk + fit_summary$ess_tail, main = "n-eff" )
+par(op)
+
+summary(fit_summary$rhat)
+summary(fit_summary$ess_bulk + fit_summary$ess_tail)
+#--------------------------------------------------------------------------
+
+#load("data_Joaquin_may20.RData")
+load("matrices_Joaquin_ag22.RData")
+library(sfsmisc)
+library(ggplot2)
+library(rstan)
+library(ggpubr)
+
+
+
+# Probability of detection ------------------------------------------------
+
+pdet <- fit_summary[grepl("ps", fit_summary$variable),]
+
+pdet <- fit_summary[grepl("ps", rownames(fit_summary)),]
+prob_det = data.frame(spp = as.character(spp),
+                      tag = as.character(tag), 
+                      probs = plogis(pdet[,1]),#convierto el fit en plogis porque est? en logit
+                      mean = pdet,
+                      q005 = pdet[,5], 
+                      q095 = pdet[,7],
+                      Neff = pdet[,9],
+                      Rhat = pdet[,10])
+write.csv(prob_det, "detection_probability_per_spp.csv", row.names = F)
+
+
+
+
+
+
+
 fit$summary("rho")
+fit$summary("betas")
